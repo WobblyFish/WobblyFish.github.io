95a59db9812a3adf07daec5494eb3044d8fb94e4
WobblyFish <96575267+WobblyFish@users.noreply.github.com>
2023-02-24 07:18:25 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 if (document.URL.substring(0, document.URL.indexOf("://")) != "https") {
-	alert("Not secure!");
+	window.location("https://" + document.URL.substring(document.URL.indexOf("://") + 3));
 }
 
 var actvHdrM = 0;
