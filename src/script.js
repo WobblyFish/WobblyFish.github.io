a961fdd05ded438fdf03dd777988f59cfc73599d
WobblyFish <96575267+WobblyFish@users.noreply.github.com>
2023-02-24 12:07:23 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -12,22 +12,24 @@ var pageArr = [
 ];
 
 function hdrMover(b) {
-	if (b) {
-		mClkThisMOver = false;
-		document.getElementById("hdr").style.animation = "0.25s openHdr 1";
-		document.getElementById("hdr").style.width = "160px";
-		for (var i = 0; i < pageArr.length; i++) {
-			document.getElementById("page_" + pageArr[i]).style.animation = "0.25s openHdrBlur 1";
-			document.getElementById("page_" + pageArr[i]).style.filter = "blur(10px)";
+	if (screen.availWidth >= 1000) {
+		if (b) {
+			mClkThisMOver = false;
+			document.getElementById("hdr").style.animation = "0.25s openHdr 1";
+			document.getElementById("hdr").style.width = "160px";
+			for (var i = 0; i < pageArr.length; i++) {
+				document.getElementById("page_" + pageArr[i]).style.animation = "0.25s openHdrBlur 1";
+				document.getElementById("page_" + pageArr[i]).style.filter = "blur(10px)";
+			}
 		}
-	}
-	else {
-		document.getElementById("hdr").style.animation = "0.25s closeHdr 1";
-		document.getElementById("hdr").style.width = "70px";
-		for (var i = 0; i < pageArr.length; i++) {
-			if (!(actvHdrM == i && mClkThisMOver)) {
-				document.getElementById("page_" + pageArr[i]).style.animation = "0.25s closeHdrBlur 1";
-				document.getElementById("page_" + pageArr[i]).style.filter = "blur(0px)";
+		else {
+			document.getElementById("hdr").style.animation = "0.25s closeHdr 1";
+			document.getElementById("hdr").style.width = "70px";
+			for (var i = 0; i < pageArr.length; i++) {
+				if (!(actvHdrM == i && mClkThisMOver)) {
+					document.getElementById("page_" + pageArr[i]).style.animation = "0.25s closeHdrBlur 1";
+					document.getElementById("page_" + pageArr[i]).style.filter = "blur(0px)";
+				}
 			}
 		}
 	}
@@ -35,19 +37,32 @@ function hdrMover(b) {
 
 function hdrSwt(n) {
 	if (actvHdrM != n) {
-		document.getElementById("page_" + pageArr[actvHdrM]).style.animation = "0.5s exitPage 1";
-		document.getElementById("page_" + pageArr[actvHdrM]).style.left = "-100%";
-		document.getElementById("page_" + pageArr[actvHdrM]).style.filter = "blur(0px)";
-		document.getElementById("hdr").style.animation = "0.25s closeHdr 1";
-		document.getElementById("hdr").style.width = "70px";
+		if (screen.availWidth >= 1000) { // Desktop
+			document.getElementById("page_" + pageArr[actvHdrM]).style.animation = "0.5s exitPage 1";
+			document.getElementById("page_" + pageArr[actvHdrM]).style.left = "-100%";
+			document.getElementById("hdr").style.animation = "0.25s closeHdr 1";
+			document.getElementById("hdr").style.width = "70px";
 
-		document.getElementById("hdrActive").style.animation = "0.5s hdrSwt_" + actvHdrM + "_" + n + " 1";
-		document.getElementById("hdrActive").style.top = (110 + (n * 70)) + "px";
+			document.getElementById("hdrActive").style.animation = "0.5s hdrSwt_" + actvHdrM + "_" + n + " 1";
+			document.getElementById("hdrActive").style.top = (110 + (n * 70)) + "px";
+
+			document.getElementById("page_" + pageArr[n]).style.animation = "0.75s enterPage 1";
+			document.getElementById("page_" + pageArr[n]).style.left = "70px";
+		}
+		else { // Mobile
+			document.getElementById("page_" + pageArr[actvHdrM]).style.animation = "0.5s m_exitPage 1";
+			document.getElementById("page_" + pageArr[actvHdrM]).style.left = "-100%";
+
+			document.getElementById("hdrActive").style.animation = "0.5s m_hdrSwt_" + actvHdrM + "_" + n + " 1";
+			if (n == 0) { document.getElementById("hdrActive").style.left = "80px"; }
+			else if (n == 1) { document.getElementById("hdrActive").style.left = "calc(33.33vw + 55px)"; }
+			else if (n == 2) { document.getElementById("hdrActive").style.left = "calc(66.66vw + 30px)"; }
+
+			document.getElementById("page_" + pageArr[n]).style.animation = "0.5s m_enterPage 1";
+			document.getElementById("page_" + pageArr[n]).style.left = "0px";
+		}
+		document.getElementById("page_" + pageArr[n]).style.filter = "blur(0px)";
 		actvHdrM = n;
 		mClkThisMOver = true;
-
-		document.getElementById("page_" + pageArr[n]).style.animation = "0.75s enterPage 1";
-		document.getElementById("page_" + pageArr[n]).style.left = "70px";
-		document.getElementById("page_" + pageArr[n]).style.filter = "blur(0px)";
 	}
 }
