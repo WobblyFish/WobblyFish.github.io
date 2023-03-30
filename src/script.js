b998c286b95123224ff42b12f297a1688ec8fe19
WobblyFish <96575267+WobblyFish@users.noreply.github.com>
2023-03-30 05:13:31 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -13,10 +13,17 @@ var pageArr = [
 
 var vHdrInsS = "<img src=\"/src/Finnimations_Icon.svg\" draggable=\"false\"><div id=\"hdrActive\"></div><ul onmouseover=\"hdrMover(true)\" onmouseout=\"hdrMover(false)\">";
 for (var i = 0; i < pageArr.length; i++) {
-	vHdrInsS += "<li onclick=\"hdrSwt(" + i + ")\" style=\"background-image:url(\'/src/" + pageArr[i].toLowerCase() + ".svg\');left: calc(" + (n * (100 / pageArr.length)) + "vw + " + (54 - (n * (56 / pageArr.length))) + "px);\">" + pageArr[i] + "</li>";
+	vHdrInsS += "<li onclick=\"hdrSwt(" + i + ")\" id=\"hdrPgLi_" + pageArr[i] + "\" style=\"background-image:url(\'/src/" + pageArr[i].toLowerCase() + ".svg\');\">" + pageArr[i] + "</li>";
 }
 vHdrInsS += "</ul>";
 document.getElementById("hdr").innerHTML = vHdrInsS;
+
+if (screen.availWidth < 1000) {
+	document.getElementById("hdrActive").style.left = "calc(" + (n * (100 / pageArr.length)) + "vw + " + (54 - (n * (56 / pageArr.length))) + "px)";
+	/*for (var i = 0; i < pageArr.length; i++) {
+		document.getElementById(hdrPgLi_" + pageArr[i]).style.left = "calc(" + (i * (100 / pageArr.length)) + "vw + " + (54 - (i * (56 / pageArr.length))) + "px);";
+	}*/
+}
 
 function hdrMover(b) {
 	if (screen.availWidth >= 1000) {
